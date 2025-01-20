4b3c791ddfababe6d7966b3a56dbb301931c17db
gb <1717634+0x6762@users.noreply.github.com>
2025-01-20 15:58:50 -0300

    Fixed expense list

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'models/expense.dart';
 import 'repositories/expense_repository.dart';
-import 'widgets/add_expense_dialog.dart';
-import 'widgets/expense_list.dart';
-import 'widgets/expense_summary.dart';
+import 'screens/expense_list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,86 +24,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: ExpenseListScreen(repository: LocalStorageExpenseRepository(prefs)),
-    );
-  }
-}
-
-class ExpenseListScreen extends StatefulWidget {
-  final ExpenseRepository repository;
-
-  const ExpenseListScreen({super.key, required this.repository});
-
-  @override
-  State<ExpenseListScreen> createState() => _ExpenseListScreenState();
-}
-
-class _ExpenseListScreenState extends State<ExpenseListScreen> {
-  List<Expense> _expenses = [];
-  late DateTime _selectedMonth;
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedMonth = DateTime(DateTime.now().year, DateTime.now().month);
-    _loadExpenses();
-  }
-
-  Future<void> _loadExpenses() async {
-    final expenses = await widget.repository.getAllExpenses();
-    setState(() {
-      _expenses = expenses;
-    });
-  }
-
-  Future<void> _addExpense() async {
-    final result = await showModalBottomSheet<Expense>(
-      context: context,
-      isScrollControlled: true,
-      useSafeArea: true,
-      builder: (context) => const AddExpenseDialog(),
-    );
-
-    if (result != null) {
-      await widget.repository.addExpense(result);
-      _loadExpenses();
-    }
-  }
-
-  void _viewExpenseDetails(Expense expense) {
-    // TODO: Implement expense details/edit
-  }
-
-  void _onMonthSelected(DateTime month) {
-    setState(() {
-      _selectedMonth = month;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Spending Tracker'),
-      ),
-      body: Column(
-        children: [
-          ExpenseSummary(
-            expenses: _expenses,
-            selectedMonth: _selectedMonth,
-            onMonthSelected: _onMonthSelected,
-          ),
-          Expanded(
-            child: ExpenseList(
-              expenses: _expenses,
-              onTap: _viewExpenseDetails,
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addExpense,
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
--- lib/screens/expense_list_screen.dart
+++ lib/screens/expense_list_screen.dart
@@ -1,43 +1,53 @@
 import 'package:flutter/material.dart';
 import '../models/expense.dart';
+import '../repositories/expense_repository.dart';
 import '../widgets/expense_list.dart';
 import '../widgets/expense_summary.dart';
 import '../widgets/add_expense_dialog.dart';
 
 class ExpenseListScreen extends StatefulWidget {
-  const ExpenseListScreen({super.key});
+  final ExpenseRepository repository;
+
+  const ExpenseListScreen({super.key, required this.repository});
 
   @override
   State<ExpenseListScreen> createState() => _ExpenseListScreenState();
 }
 
 class _ExpenseListScreenState extends State<ExpenseListScreen> {
-  final List<Expense> _expenses = [];
-  DateTime _selectedMonth = DateTime(
-    DateTime.now().year,
-    DateTime.now().month,
-  );
+  List<Expense> _expenses = [];
+  late DateTime _selectedMonth;
 
-  void _addExpense() async {
-    final expense = await showDialog<Expense>(
+  @override
+  void initState() {
+    super.initState();
+    _selectedMonth = DateTime(DateTime.now().year, DateTime.now().month);
+    _loadExpenses();
+  }
+
+  Future<void> _loadExpenses() async {
+    final expenses = await widget.repository.getAllExpenses();
+    setState(() {
+      _expenses = expenses;
+    });
+  }
+
+  Future<void> _addExpense() async {
+    final result = await showModalBottomSheet<Expense>(
       context: context,
-      builder: (ctx) => const AddExpenseDialog(),
+      isScrollControlled: true,
+      useSafeArea: true,
+      builder: (context) => const AddExpenseDialog(),
     );
 
-    if (expense != null) {
-      setState(() {
-        _expenses.add(expense);
-        // Update selected month to the month of the new expense if it's different
-        if (expense.date.year != _selectedMonth.year ||
-            expense.date.month != _selectedMonth.month) {
-          _selectedMonth = DateTime(expense.date.year, expense.date.month);
-        }
-      });
+    if (result != null) {
+      await widget.repository.addExpense(result);
+      _loadExpenses();
     }
   }
 
   void _viewExpenseDetails(Expense expense) {
-    // TODO: Implement expense details view
+    // TODO: Implement expense details/edit
   }
 
   void _onMonthSelected(DateTime month) {
@@ -50,7 +60,7 @@ class _ExpenseListScreenState extends State<ExpenseListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Expense Tracker'),
+        title: const Text('Spending Tracker'),
       ),
       body: Column(
         children: [
@@ -61,7 +71,11 @@ class _ExpenseListScreenState extends State<ExpenseListScreen> {
           ),
           Expanded(
             child: ExpenseList(
-              expenses: _expenses,
+              expenses: _expenses
+                  .where((expense) =>
+                      expense.date.year == _selectedMonth.year &&
+                      expense.date.month == _selectedMonth.month)
+                  .toList(),
               onTap: _viewExpenseDetails,
             ),
           ),
