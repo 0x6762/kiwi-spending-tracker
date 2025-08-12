c3a725a7e0159e176e3ea94fa74221d9f5306a48
0x6762 <1717634+0x6762@users.noreply.github.com>
2025-08-12 18:33:16 -0300

    Added animation on scroll

--- lib/widgets/navigation/bottom_navigation.dart
+++ lib/widgets/navigation/bottom_navigation.dart
@@ -22,9 +22,54 @@ class BottomNavigation extends StatefulWidget {
   State<BottomNavigation> createState() => BottomNavigationState();
 }
 
-class BottomNavigationState extends State<BottomNavigation> {
+class BottomNavigationState extends State<BottomNavigation>
+    with SingleTickerProviderStateMixin {
   bool _isVisible = true;
-  static const Duration _animationDuration = Duration(milliseconds: 200);
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _slideAnimation;
+  late Animation<double> _scaleAnimation;
+
+  static const Duration _animationDuration = Duration(milliseconds: 250);
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: _animationDuration,
+      vsync: this,
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutCubic,
+    ));
+
+    _slideAnimation = Tween<double>(
+      begin: 0.0,
+      end: 50.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutCubic,
+    ));
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 0.96,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOutCubic,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   // Expose methods for external control
   void showNavigation() {
@@ -32,6 +77,7 @@ class BottomNavigationState extends State<BottomNavigation> {
       setState(() {
         _isVisible = true;
       });
+      _animationController.reverse();
       widget.onShow?.call();
     }
   }
@@ -41,23 +87,31 @@ class BottomNavigationState extends State<BottomNavigation> {
       setState(() {
         _isVisible = false;
       });
+      _animationController.forward();
       widget.onHide?.call();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: _animationDuration,
-      curve: Curves.easeInOut,
-      child: Transform.translate(
-        offset: Offset(0, _isVisible ? 0 : 200), // Move 200px down when hidden
-        child: AppBottomNavigationBar(
-          items: widget.items,
-          selectedIndex: widget.selectedIndex,
-          onDestinationSelected: widget.onDestinationSelected,
-        ),
-      ),
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, _slideAnimation.value),
+          child: Transform.scale(
+            scale: _scaleAnimation.value,
+            child: Opacity(
+              opacity: _fadeAnimation.value,
+              child: AppBottomNavigationBar(
+                items: widget.items,
+                selectedIndex: widget.selectedIndex,
+                onDestinationSelected: widget.onDestinationSelected,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
